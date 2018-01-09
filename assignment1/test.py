45db2b3e19a3c007a57e84facd01a66bd67ce2cc
XIANGFANGCODE <fang.x@outlook.com>
2018-01-09 21:42:54 +0800

    Merge branch 'master' of https://github.com/XIANGFANGCODE/cs231_assignment

--- assignment1/test.py
+++ assignment1/test.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-A = np.arange(95,99).reshape(2,2)
-print (A)
-pad = 1
-A = np.pad(A,((pad,pad),(pad,pad)),'constant',constant_values = (0,0))
-print (A)
+print (4//2)
 
-print (A[:,:])
-
+print (7//2)
