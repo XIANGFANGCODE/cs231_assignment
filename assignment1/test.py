2ad5ce9a64940632b3dea83c8b013daabad638ea
XIANGFANGCODE <fang.x@outlook.com>
2018-01-14 21:56:10 +0800

    update tensorflow

--- assignment1/test.py
+++ assignment1/test.py
@@ -4,9 +4,5 @@ import numpy as np
 import math
 import timeit
 
-c = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-print(c)
-s = c.shape
-print(c.shape)
-l = s.as_list()
-print(l)
+f = np.can_cast(np.float64, np.float32)
+print (f)
