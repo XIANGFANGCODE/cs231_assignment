467d694bf8aca99abd07b480c96814b4a4586abc
FangXiang <fang.x@outlook.com>
2018-02-22 10:28:16 +0800

    update lstm

--- assignment1/test.py
+++ assignment1/test.py
@@ -1,8 +1,22 @@
-#import numpy as np
-import tensorflow as tf
-import numpy as np
-import math
-import timeit
-
-for i in range(9-1, -1, -1):
-    print(i)
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        if root is None:
+            return None
+        if root.left is None and root.right is None:
+            return None
+        _out = list()
+        _out.append(root)
+        for node in _out:
+            if node.left is not None:
+                _out.append(node.left)
+            if node.right is not None:
+                _out.append(node.right)
+        return _out
--- assignment3/cs231n/image_utils.py
+++ assignment3/cs231n/image_utils.py
@@ -60,8 +60,8 @@ def image_from_url(url):
     """
     try:
         f = urllib.request.urlopen(url)
-        _, fname = tempfile.mkstemp()
-        # fname = "E://temp"
+        #_, fname = tempfile.mkstemp()
+        fname = "E://temp"
         with open(fname, 'wb') as ff:
             ff.write(f.read())
         img = imread(fname)
